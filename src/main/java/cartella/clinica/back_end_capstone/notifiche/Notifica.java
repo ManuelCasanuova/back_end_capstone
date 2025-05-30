259d39ec0032a683f265eddffdb0f9e4269fd563
ManuelCasanuova <manuelcasanuova@gmail.com>
2025-05-30 12:33:11 +0200

    creazione della classe notifiche

---
+++ src/main/java/cartella/clinica/back_end_capstone/notifiche/Notifica.java
@@ -0,0 +1,32 @@
+package cartella.clinica.back_end_capstone.notifiche;
+
+import cartella.clinica.back_end_capstone.auth.AppUser;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "notifiche")
+
+public class Notifica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String messaggio;
+
+    private boolean letta = false;
+
+    private LocalDateTime dataCreazione = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "destinatario_id", nullable = false)
+    private AppUser destinatario;
+
+}
